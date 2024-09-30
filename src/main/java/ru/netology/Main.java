3ded7f4a0c3f0e3040ec174ae9a0a0b1cb698242
dogmou <trup.lexa@gmail.com>
2024-09-30 17:29:30 +0500

    Initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        int bonus = (int) service.calculate(5_000, true);
+        System.out.println(bonus);
+    }
+}
+
